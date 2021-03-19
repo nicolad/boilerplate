0ae48d0e63cad7bfa1470abbb129144c6ebadf3b
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-03-19 15:26:17 +0200

    Small improvement.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bla bla to reload</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn anything
         </a>
       </header>
     </div>
